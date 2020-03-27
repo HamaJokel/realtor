5e505cc622ee601a646c17ed5100daa9be058e88
Hama <jokelhama@gmail.com>
2020-03-27 18:16:25 +0100

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,4 +149,9 @@ EMAIL_HOST_USER = 'noreplyhamanoreply@gmail.com'
 EMAIL_HOST_PASSWORD = 'fotboll123'
 EMAIL_USE_TLS=True 
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 
